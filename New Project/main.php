495646bdb66cb228daba600dfd976403af90d6e2
Mukesh Ashtekar <ashtekarmukesh@gmail.com>
2014-12-19 23:59:13 +0530

    Created by Coding Ground Client

---
+++ New Project/main.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
+<pre>
+<?php
+  var_dump(explode(";","001818.208;Karton ;Carton ;Cartoni ;Box ;Hero Delicia Heidelbeerkonfitüre ;Hero Delicia Confiture de myrtilles ;Hero Delicia Confettura di mirtilli ; ;Hero Delicia Heidelbeerkonfitüre GLA 320g;Hero Delicia Confiture de myrtilles VER 320g;Hero Delicia Confettura di mirtilli VET 320g; ;Glas 320g Delicia ;Bocal 320 g Delicia ;Vetro 320 g G ; ; 0.320 ; 0.540 ;0.000 G ;1 Stück = 1 Stück ;1 Karton = 8 Stück ;1 Lagen = 112 Stück ;1 Palette = 896 Stück ;1 Pièce = 1 Pièce ;1 Carton = 8 Pièce ;1 Lagen = 112 Pièce ;1 Palette = 896 Pièce ;1 Pezzi = 1 Pezzi ;1 Cartoni = 8 Pezzi ;1 Lagen = 112 Pezzi ; 1 Palette = 896 Pezzi ; ;1 Box = 8 Pezzi ;1 layer = 112 Pezzi ;1 Pallet = 896 Pezzi ;00/12/00;UK/18-28°C ;UK/18-28°C ;UK/18-28°C ;UK/18-28°C ;7614200093699;7614200093705;900693 ; ;618027* ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 4.70 ; 3.84 ; 3.82 ; ; ;00"));
+?>
+</pre>
+</body>
+</html>
